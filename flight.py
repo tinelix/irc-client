65c8ae27912d5ad11c04deeca543a76a0f4d966e
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2021-08-23 10:51:33 +0700

    Ver. 0.0.1 (alpha, 2021-08-23)

---
+++ flight.py
@@ -0,0 +1,319 @@
+import sys, PyQt5, dlg001, configparser, time, threading, socket
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from mainform import Ui_MainWindow
+from dlg001 import Ui_Dialog as swiz_001
+from dlg002 import Ui_Dialog as swiz_002
+from dlg003 import Ui_Dialog as swiz_003
+
+settings = configparser.ConfigParser()
+profiles = configparser.ConfigParser()
+
+class Thread(QThread):
+    logged = QtCore.pyqtSignal(str)
+    started = QtCore.pyqtSignal(str)
+
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+    def run(self):
+            profiles.read('profiles')
+            if profiles.sections() != [] or profiles.sections() != None and (profiles[self.parent.ui.tableWidget.item(self.parent.ui.tableWidget.currentRow(), 0).text()]['server'] != '' and profiles[self.parent.ui.tableWidget.item(self.parent.ui.tableWidget.currentRow(), 0).text()]['port'] != '') and (profiles[self.parent.ui.tableWidget.item(self.parent.ui.tableWidget.currentRow(), 0).text()]['nicknames'] != '' and profiles[self.parent.ui.tableWidget.item(self.parent.ui.tableWidget.currentRow(), 0).text()]['nicknames'] != ''):
+                self.encoding = profiles[self.parent.ui.tableWidget.item(self.parent.ui.tableWidget.currentRow(), 0).text()]['Encoding']
+                self.username = profiles[self.parent.ui.tableWidget.item(self.parent.ui.tableWidget.currentRow(), 0).text()]['Nicknames'].split(', ')[0]
+                self.server = profiles[self.parent.ui.tableWidget.item(self.parent.ui.tableWidget.currentRow(), 0).text()]['Server']
+                self.port = int(profiles[self.parent.ui.tableWidget.item(self.parent.ui.tableWidget.currentRow(), 0).text()]['Port'])
+                self.channel = ""
+                self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                try:
+                    self.socket.connect((self.server,self.port))
+                    print('Connecting to {0}...'.format(self.server))
+                    self.socket.setblocking(False)
+                    self.socket.send(bytes("USER " + self.username + " " + self.username +" " + self.username + " :Testing\n", self.encoding))
+                    self.socket.send(bytes("NICK " + self.username + "\n", self.encoding))
+                    #self.socket.send(bytes("NICKSERV IDENTIFY " + self.username + " " + self.password + "\n", self.encoding))
+                    while True:
+                        time.sleep(2)
+                        self.text=self.socket.recv(2040)
+                        self.started.emit(self.text.decode())
+
+                except Exception as e:
+                    print('Exception: {0}'.format(e))
+                    self.started.emit('Exception: {0}'.format(str(e)))
+
+    def kill(self):
+        self.quit()
+
+class mainform(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(mainform, self).__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.conn_quality_progr.setValue(0)
+        self.ui.latency_label.setText("No signal")
+        profiles.read('profiles')
+        self.child = SettingsWizard001(self)
+        settings.read('settings')
+        profiles.read('profiles')
+        print('Tinelix codename Flight v. 0.0.1 (alpha) (2021-08-23)\nDone!')
+
+        if settings.sections() == [] or profiles.sections() == []:
+            settings['Main'] = {'Language': 'Russian', 'ColorScheme': 'Orange', 'DarkTheme': 'Enabled'}
+            with open('settings', 'w') as configfile:
+                settings.write(configfile)
+
+        #swiz001 = SettingsWizard001()
+        self.child.show()
+
+
+class SettingsWizard003(QtWidgets.QDialog):
+
+    def __init__(self):
+        super().__init__()
+        self.ui = swiz_003()
+        self.ui.setupUi(self)
+        self.ui.buttonBox.accepted.connect(self.save_profile)
+        self.ui.buttonBox.accepted.connect(self.save_profile)
+        self.ui.clear_nicknames_btn.clicked.connect(self.clear_nicknames)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.tick)
+        self.timer.start(200)
+
+    def tick(self):
+        self.ui.nicknames_combo.currentIndexChanged.connect(self.show_create_nickname_dlg)
+        self.timer.stop()
+
+    def show_create_nickname_dlg(self):
+        print(self.ui.nicknames_combo.count())
+        index = self.ui.nicknames_combo.currentIndex()
+        if index == self.ui.nicknames_combo.count() - 1:
+            self.close()
+            swiz002 = SettingsWizard002()
+            swiz002.ui.label.setText('Никнейм:')
+            swiz002.ui.profname.setText(self.ui.profname_box.text())
+            swiz002.exec_()
+
+    def add_nickname(self):
+        self.ui.nicknames_combo.addItem(self.ui.lineEdit.text())
+
+    def save_profile(self):
+        try:
+            profiles.read('profilles')
+            nicknames_list = []
+            for index in range(self.ui.nicknames_combo.count()):
+                if index < (self.ui.nicknames_combo.count() - 1):
+                    nicknames_list.append(self.ui.nicknames_combo.itemText(self.ui.nicknames_combo.count()))
+            profiles[str(self.ui.profname_box.text())] = {'AuthMethod': '', 'Nicknames': profiles[str(self.ui.profname_box.text())]['Nicknames'], 'Server': self.ui.server_box.text(), 'Port': str(self.ui.port_box.value()), 'Encoding': self.ui.encoding_combo.currentText(), 'QuitingMsg': self.ui.quiting_msg_box.text()}
+            with open('profiles', 'w') as configfile:
+                profiles.write(configfile)
+        except Exception as e:
+                print(e)
+
+    def clear_nicknames(self):
+        self.ui.nicknames_combo.currentIndexChanged.disconnect()
+        self.ui.nicknames_combo.clear()
+        self.ui.nicknames_combo.addItem('')
+        try:
+            profiles[str(self.ui.profname_box.text())]['Nicknames'] = ''
+            with open('profiles', 'w') as configfile:
+                profiles.write(configfile)
+        except Exception as e:
+                print(e)
+        self.ui.nicknames_combo.addItem('Создать никнейм...')
+        self.ui.clear_nicknames_btn.setEnabled(False)
+        self.ui.clear_nicknames_btn.setStyleSheet('border-color: rgb(255, 119, 0); selection-background-color: rgb(255, 119, 0); color: #4f4f4f')
+        self.timer.start(200)
+
+class SettingsWizard002(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = swiz_002()
+        self.ui.setupUi(self)
+        self.ui.buttonBox.accepted.connect(self.show_custom_edit_dlg)
+
+    def show_custom_edit_dlg(self):
+        profiles.read('profiles')
+        if profiles.sections() == []:
+            profiles[str(self.ui.lineEdit.text())] = {'AuthMethod': '', 'Nicknames': '', 'Server': '', 'Port': '', 'Encoding': '', 'QuitingMsg': ''}
+            with open('profiles', 'w') as configfile:
+                profiles.write(configfile)
+        if self.ui.label.text() == '<html><head/><body><p>Имя профиля:</p></body></html>':
+            swiz003 = SettingsWizard003()
+            swiz003.ui.title_label.setText(str(self.ui.lineEdit.text()))
+            swiz003.ui.profname_box.setText(str(self.ui.lineEdit.text()))
+            profiles.read('profiles')
+            try:
+                if profiles.sections() != [] and profiles[str(self.ui.lineEdit.text())]['Nicknames'] != "" and profiles[str(self.ui.lineEdit.text())]['Nicknames'] != " " and profiles[str(self.ui.lineEdit.text())]['Nicknames'] != None:
+                    for nick in list(profiles[str(self.ui.lineEdit.text())]['Nicknames'].split(", ")):
+                        if nick != "" and nick != " ":
+                            swiz003.ui.nicknames_combo.addItem(nick)
+                    swiz003.ui.server_box.setText(profiles[str(self.ui.lineEdit.text())]['Server'])
+                    swiz003.ui.port_box.setValue(int(profiles[str(self.ui.lineEdit.text())]['Port']))
+                    swiz003.ui.quiting_msg_box.setText(profiles[str(self.ui.lineEdit.text())]['quitingmsg'])
+                else:
+                    swiz003.ui.nicknames_combo.addItem('')
+                    swiz003.ui.clear_nicknames_btn.setEnabled(False)
+                    swiz003.ui.clear_nicknames_btn.setStyleSheet('color: #4f4f4f')
+            except Exception as e:
+                if swiz003.ui.nicknames_combo.count() == 0:
+                    swiz003.ui.nicknames_combo.addItem('')
+            swiz003.ui.nicknames_combo.addItem('Создать никнейм...')
+            swiz003.ui.encoding_combo.addItem('UTF-8')
+            swiz003.ui.encoding_combo.addItem('Windows-1251')
+            swiz003.ui.encoding_combo.addItem('DOS (866)')
+            try:
+                swiz003.ui.encoding_combo.setCurrentText(profiles[str(self.ui.lineEdit.text())]['encoding'])
+            except Exception as e:
+                print(e)
+            swiz003.exec_()
+        elif self.ui.label.text() == 'Никнейм:':
+            swiz003 = SettingsWizard003()
+            profiles.read('profiles')
+            try:
+                if profiles[str(self.ui.profname.text())]['Nicknames'] != "" and profiles[str(self.ui.profname.text())]['Nicknames'] != " ":
+                    profiles[str(self.ui.profname.text())]['Nicknames'] = profiles[str(self.ui.profname.text())]['Nicknames'] + ', ' + self.ui.lineEdit.text()
+                    with open('profiles', 'w') as configfile:
+                        profiles.write(configfile)
+                else:
+                     profiles[str(self.ui.profname.text())]['Nicknames'] = self.ui.lineEdit.text()
+                     with open('profiles', 'w') as configfile:
+                        profiles.write(configfile)
+                for nick in list(profiles[str(self.ui.profname.text())]['Nicknames'].split(", ")):
+                    if nick != "" and nick != " ":
+                        swiz003.ui.nicknames_combo.addItem(nick)
+            except Exception as e:
+                print(e)
+            swiz003.ui.title_label.setText(str(self.ui.profname.text()))
+            swiz003.ui.profname_box.setText(str(self.ui.profname.text()))
+            swiz003.ui.nicknames_combo.addItem('Создать никнейм...')
+            swiz003.ui.encoding_combo.addItem('UTF-8')
+            swiz003.ui.encoding_combo.addItem('Windows-1251')
+            swiz003.ui.encoding_combo.addItem('DOS (866)')
+            swiz003.exec_()
+
+class SettingsWizard001(QtWidgets.QDialog, swiz_001):
+    def __init__(self, parent=None):
+        super(SettingsWizard001, self).__init__(parent)
+        self.ui = swiz_001()
+        self.ui.setupUi(self)
+        self.ui.language_combo.addItem('Russian')
+        self.ui.language_combo.addItem('English')
+        self.ui.add_profile_btn.clicked.connect(self.show_custom_edit_dlg)
+        self.ui.connect_btn.clicked.connect(self.irc_connect)
+        self.ui.change_profile_btn.clicked.connect(self.edit_item)
+        self.ui.del_profile_btn.clicked.connect(self.del_item)
+        settings.read('settings')
+        profiles.read('profiles')
+        self.ui.tableWidget.setRowCount(1);
+        self.ui.tableWidget.setColumnCount(2);
+        header = self.ui.tableWidget.horizontalHeader()
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+        self.ui.tableWidget.itemClicked.connect(self.click_item)
+        self.ui.tableWidget.itemDoubleClicked.connect(self.edit_item)
+        self.ui.connect_btn.setEnabled(False)
+        self.ui.connect_btn.setStyleSheet('color: #4f4f4f')
+        self.ui.change_profile_btn.setEnabled(False)
+        self.ui.change_profile_btn.setStyleSheet('color: #4f4f4f')
+        self.ui.del_profile_btn.setEnabled(False)
+        self.ui.del_profile_btn.setStyleSheet('color: #4f4f4f')
+        self.parent = parent
+
+        try:
+            if profiles.sections() != [] or profiles.sections() != None:
+                self.ui.tableWidget.setRowCount(0);
+                rowPosition = self.ui.tableWidget.rowCount()
+                self.ui.tableWidget.insertRow(rowPosition)
+                for section in profiles.sections():
+                    self.ui.tableWidget.setItem(rowPosition, 0, QTableWidgetItem(section))
+                    if profiles[section]['Server'] != '':
+                        self.ui.tableWidget.setItem(rowPosition, 1, QTableWidgetItem('{0}:{1}'.format(profiles[section]['Server'], profiles[section]['Port'])))
+                    else:
+                        self.ui.tableWidget.setItem(rowPosition, 1, QTableWidgetItem('-'))
+        except:
+            pass
+
+    def show_custom_edit_dlg(self):
+        swiz002 = SettingsWizard002()
+        swiz002.exec_()
+
+    def irc_connect(self):
+        self.thread = Thread(self)
+        self.thread.started.connect(self.started)
+        self.thread.start()
+
+
+    @QtCore.pyqtSlot(str)
+    def started(self, status):
+        text = '{}'.format(status)
+        self.parent.ui.chat_text.setPlainText('{0}\n{1}'.format(self.parent.ui.chat_text.toPlainText(), text))
+
+
+    def edit_item(self):
+        swiz003 = SettingsWizard003()
+        swiz003.ui.title_label.setText(str(self.ui.tableWidget.item(self.ui.tableWidget.currentRow(), 0).text()))
+        swiz003.ui.profname_box.setText(str(self.ui.tableWidget.item(self.ui.tableWidget.currentRow(), 0).text()))
+        profiles.read('profiles')
+        try:
+            if profiles.sections() != [] and profiles[str(self.ui.tableWidget.item(self.ui.tableWidget.currentRow(), 0).text())]['Nicknames'] != "" and profiles[str(self.ui.tableWidget.item(self.ui.tableWidget.currentRow(), 0).text())]['Nicknames'] != " " and profiles[str(self.ui.tableWidget.item(self.ui.tableWidget.currentRow(), 0).text())]['Nicknames'] != None:
+                for nick in list(profiles[str(self.ui.tableWidget.item(self.ui.tableWidget.currentRow(), 0).text())]['Nicknames'].split(", ")):
+                    if nick != "" and nick != " ":
+                        swiz003.ui.nicknames_combo.addItem(nick)
+                swiz003.ui.server_box.setText(profiles[str(self.ui.tableWidget.item(self.ui.tableWidget.currentRow(), 0).text())]['Server'])
+                swiz003.ui.port_box.setValue(int(profiles[str(self.ui.tableWidget.item(self.ui.tableWidget.currentRow(), 0).text())]['Port']))
+                swiz003.ui.quiting_msg_box.setText(profiles[str(self.ui.tableWidget.item(self.ui.tableWidget.currentRow(), 0).text())]['quitingmsg'])
+            else:
+                swiz003.ui.nicknames_combo.addItem('')
+                swiz003.ui.clear_nicknames_btn.setEnabled(False)
+                swiz003.ui.clear_nicknames_btn.setStyleSheet('color: #4f4f4f')
+        except Exception as e:
+            if swiz003.ui.nicknames_combo.count() == 0:
+                swiz003.ui.nicknames_combo.addItem('')
+        swiz003.ui.nicknames_combo.addItem('Создать никнейм...')
+        swiz003.ui.encoding_combo.addItem('UTF-8')
+        swiz003.ui.encoding_combo.addItem('Windows-1251')
+        swiz003.ui.encoding_combo.addItem('DOS (866)')
+        try:
+            swiz003.ui.encoding_combo.setCurrentText(profiles[str(self.ui.tableWidget.item(self.ui.tableWidget.currentRow(), 0).text())]['encoding'])
+        except Exception as e:
+            print(e)
+        swiz003.exec_()
+
+    def del_item(self):
+        try:
+            profiles.remove_section(str(self.ui.tableWidget.item(self.ui.tableWidget.currentRow(), 0).text()))
+            with open('profiles', 'w') as configfile:
+                profiles.write(configfile)
+        except Exception as e:
+            print(e)
+        self.ui.tableWidget.removeRow(self.ui.tableWidget.currentRow())
+        if self.ui.tableWidget.rowCount() == 0:
+            self.ui.tableWidget.itemDoubleClicked.connect(self.edit_item)
+            self.ui.connect_btn.setEnabled(False)
+            self.ui.connect_btn.setStyleSheet('border-color: rgb(255, 119, 0); selection-background-color: rgb(255, 119, 0); color: #4f4f4f')
+            self.ui.change_profile_btn.setEnabled(False)
+            self.ui.change_profile_btn.setStyleSheet('border-color: rgb(255, 119, 0); selection-background-color: rgb(255, 119, 0); color: #4f4f4f')
+            self.ui.del_profile_btn.setEnabled(False)
+            self.ui.del_profile_btn.setStyleSheet('border-color: rgb(255, 119, 0); selection-background-color: rgb(255, 119, 0); color: #4f4f4f')
+
+    def click_item(self):
+        profiles.read('profiles')
+        self.ui.change_profile_btn.setEnabled(True)
+        self.ui.change_profile_btn.setStyleSheet('border-color: rgb(255, 119, 0); selection-background-color: rgb(255, 119, 0); color: #ffffff')
+        self.ui.del_profile_btn.setEnabled(True)
+        self.ui.del_profile_btn.setStyleSheet('border-color: rgb(255, 119, 0); selection-background-color: rgb(255, 119, 0); color: #ffffff')
+        try:
+            if profiles[self.ui.tableWidget.item(self.ui.tableWidget.currentRow(), 0).text()]['server'] != '' and profiles[self.ui.tableWidget.item(self.ui.tableWidget.currentRow(), 0).text()]['port'] != '':
+                self.ui.connect_btn.setEnabled(True)
+                self.ui.connect_btn.setStyleSheet('border-color: rgb(255, 119, 0); selection-background-color: rgb(255, 119, 0); color: #ffffff')
+        except:
+            pass
+
+app = QtWidgets.QApplication([])
+app.setStyle('Fusion')
+application = mainform()
+application.show()
+
+
+sys.exit(app.exec())
